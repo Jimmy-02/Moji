4ca48601b07674cf062fa19bf7f01ee7a928ab0c
Jimmy-02 <tainat1202@gmail.com>
2026-01-28 17:07:34 +0700

    feat: implement sign-in API

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,19 +1,25 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import Session from "../models/Session.js";
+import dotenv from 'dotenv'
 
+dotenv.config();
+
+const ACCESS_TOKEN_TTL = process.env.ACCESS_TOKEN_TTL;
+const REFRESH_TOKEN_TTL = Number(process.env.REFRESH_TOKEN_TTL);
 export const signUp = async (req, res) => {
   try {
     const { username, password, email, firstName, lastName } = req.body;
     if (!username || !password || !email || !firstName || !lastName) {
-      return res
-        .status(400)
-        .json({
-          message:
-            "cannot missing username, password, email, firstName, lastName",
-        });
+      return res.status(400).json({
+        message:
+          "cannot missing username, password, email, firstName, lastName",
+      });
     }
 
-    const duplicate = await User.findOne({username})
+    const duplicate = await User.findOne({ username });
 
     if (duplicate) {
       return res.status(409).json({ message: "username exist" });
@@ -22,16 +28,62 @@ export const signUp = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await User.create({
-        username,
-        hashedPassword,
-        email,
-        displayName: `${firstName} ${lastName}`
+      username,
+      hashedPassword,
+      email,
+      displayName: `${firstName} ${lastName}`,
     });
 
-    return res.sendStatus(204)
+    return res.sendStatus(204);
+  } catch (error) {
+    console.log("error when call signUp", error);
+    return res.status(500).json({ message: "System error" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    
+    const {username, password} = req.body;
+
+    if(!username || !password){
+      return res.status(400).json({message: "Missing username or password"});
+    }
+
+    const user = await User.findOne({username});
+
+    if(!user){
+      return res.status(401).json({message: "Wrong username or password"});
+    }
+
+    const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
+
+    if(!passwordCorrect){
+      return res.status(401).json({message: "Wrong username or password"});
+    }
+
+    const accessToken = jwt.sign({userId: user._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: ACCESS_TOKEN_TTL});
+
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    return res.status(200).json({message: `User ${user.displayName} is logged in!`, accessToken})
 
   } catch (error) {
-    console.log('error when call signUp', error);
-    return res.status(500).json({message: 'System error'});
+    console.log("error when call signIp", error);
+    return res.status(500).json({ message: "System error" });
   }
+
 };
