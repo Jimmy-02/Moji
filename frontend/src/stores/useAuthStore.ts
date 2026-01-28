545951bc027b92d0e1ecb1b96f96a504bf60af6c
Jimmy-02 <tainat1202@gmail.com>
2026-01-28 23:23:45 +0700

    feat: implement UI and logic for sign-in and sign-up

---
+++ frontend/src/stores/useAuthStore.ts
@@ -0,0 +1,27 @@
+import {create} from 'zustand'
+import {toast} from 'sonner'
+import { authService } from '@/services/authService';
+import type { AuthState } from '@/types/store';
+
+export const useAuthStore = create<AuthState>((set, get) =>({
+    accessToken: null,
+    user: null,
+    loading: false,
+
+    signUp: async (username, password, email, firstName, lastName) =>{
+        try {
+            set({loading: true})
+
+            await authService.signUp(username, password, email, firstName, lastName);
+
+            toast.success('Đăng ký thành công! Bạn sẽ được chuyển sang trang đăng nhập.');
+        } catch (error) {
+            console.log(error);
+            toast.error('Đăng ký không thành công');
+        }
+        finally{
+            set({loading: false});
+        }
+        
+    },
+}));
