e7ee34d61717f2cfd277a2b22e0db4fc3d2e883f
Jimmy-02 <tainat1202@gmail.com>
2026-01-31 23:20:12 +0700

    feat: innitialize friend controller

---
+++ backend/src/controllers/friendController.js
@@ -0,0 +1,39 @@
+export const addFriend = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const acceptFriendRequest = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const declineFriendRequest = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const getAllFriends = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const getFriendRequest = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
