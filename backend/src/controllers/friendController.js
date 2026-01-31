f0340dfce0f838d90a01ac0f36213a238f8887b7
Jimmy-02 <tainat1202@gmail.com>
2026-01-31 23:44:38 +0700

    feat: implement accept friend request

--- backend/src/controllers/friendController.js
+++ backend/src/controllers/friendController.js
@@ -59,9 +59,40 @@ export const sendFriendRequest = async(req, res)=>{
 
 export const acceptFriendRequest = async(req, res)=>{
     try {
-        
+        const {requestId} = req.params;
+        const userId = req.user._id;
+
+        const request = await FriendRequest.findById(requestId);
+
+    if (!request) {
+        return res.status(404).json({ message: "Friend request not found!" });
+    }
+
+    if (request.to.toString() !== userId.toString()) {
+        return res.status(403).json({ message: "You cannot accept this friend request!" });
+    }
+
+    const friend = await Friend.create({
+        userA: request.from,
+        userB: request.to
+    })
+
+    await FriendRequest.findByIdAndDelete(requestId);
+
+    const from = await User.findById(request.from).select("_id displayName avatarUrl").lean();
+
+    return res.status(200).json({
+      message: "Friend request accepted!",
+      newFriend: {
+        _id: from?._id,
+        displayName: from?.displayName,
+        avatarUrl: from?.avatarUrl,
+      },
+    });
+
     } catch (error) {
-        
+        console.error("Error when accepting friend request", error);
+        return res.status(500).json({ message: "System error" });
     }
 }
 
