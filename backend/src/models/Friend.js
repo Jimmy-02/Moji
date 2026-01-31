2f74e6ddadb28b40f3bb82b949ed51b2c28c54e4
Jimmy-02 <tainat1202@gmail.com>
2026-02-01 00:22:24 +0700

    fix: remove next() because its not a function

--- backend/src/models/Friend.js
+++ backend/src/models/Friend.js
@@ -18,7 +18,7 @@ const friendSchema = new mongoose.Schema(
   }
 );
 
-friendSchema.pre("save", function (next) {
+friendSchema.pre("save", function () {
   const a = this.userA.toString();
   const b = this.userB.toString();
 
@@ -27,7 +27,6 @@ friendSchema.pre("save", function (next) {
     this.userB = new mongoose.Types.ObjectId(a);
   }
 
-  next();
 });
 
 friendSchema.index({ userA: 1, userB: 1 }, { unique: true });
