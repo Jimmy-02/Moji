3b5404061351273f42c28b0e1fdf9def13db7d47
Jimmy-02 <tainat1202@gmail.com>
2026-02-06 05:10:14 +0700

    feat: extend friendship check to support group members

--- backend/src/middlewares/friendMiddleware.js
+++ backend/src/middlewares/friendMiddleware.js
@@ -6,6 +6,11 @@ export const checkFriendship = async (req, res, next) => {
     try {
         const me = req.user._id.toString();
         const recipientId = req.body?.recipientId ?? null;
+        const memberIds = req.body?.memberIds ?? [];
+
+        if (!recipientId && memberIds.length === 0) {
+          return res.status(400).json({ message: "Need to provide recipientId or memberIds" });
+        }
 
         if (recipientId) {
           const [userA, userB] = pair(me, recipientId);
@@ -20,6 +25,26 @@ export const checkFriendship = async (req, res, next) => {
 
           return next();
         }
+
+        const friendChecks = memberIds.map(async (memberId) => {
+          const [userA, userB] = pair(me, memberId);
+          const friend = await Friend.findOne({ userA, userB });
+          return friend ? null : memberId; //null if they are friend
+        });
+
+        const results = await Promise.all(friendChecks);
+        const notFriends = results.filter(Boolean); //filter true -> memberid (not friend)
+
+        if (notFriends.length > 0) {
+          return res
+            .status(403)
+            .json({
+              message: "You can add your friend to your group.",
+              notFriends,
+            });
+        }
+
+        next();
     } catch (error) {
         console.error("Error when checking friend ship", error);
         return res.status(500).json({ message: "System error" });
