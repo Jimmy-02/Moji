16b9dbe1272eedb9ded5e59c6b53a6ab97be6d2e
Jimmy-02 <tainat1202@gmail.com>
2026-02-07 09:41:07 +0700

    feat: initialize side bar

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,14 +1,6 @@
-import Logout from '@/components/auth/logout'
-import { useAuthStore } from '@/stores/useAuthStore'
 
 const ChatAppPage = () => {
-    const user = useAuthStore((s) => s.user);
-    return (
-        <div>
-            {user?.username}
-            <Logout></Logout>
-        </div>
-    )
-}
+    
+};
 
 export default ChatAppPage
