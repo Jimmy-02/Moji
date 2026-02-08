afabf745d292d603842cc377f20098fa8d925c5d
Jimmy-02 <tainat1202@gmail.com>
2026-02-08 10:07:26 +0700

    feat: implement sidebar header

--- frontend/src/components/sidebar/app-sidebar.tsx
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -8,28 +8,44 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Moon, Sun } from "lucide-react"
+import { Switch } from "../ui/switch";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
   return (
     <Sidebar variant="inset" {...props}>
       {/* Header */}
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild className="bg-gradient-primary">
-
+            <SidebarMenuButton
+              size="lg"
+              asChild
+              className="bg-gradient-primary"
+            >
+              <a href="#">
+                <div className="flex w-full items-center px-2 justify-between">
+                  <h1 className="text-xl font-bold text-white">Moji</h1>
+                  <div className="flex items-center gap-2">
+                    <Sun className="size-4 text-white/80" />
+                    <Switch
+                      checked={true}
+                      onCheckedChange={() => {}}
+                      className="data-[state=checked]:bg-background/80"
+                    />
+                    <Moon className="size-4 text-white/80" />
+                  </div>
+                </div>
+              </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       {/* Content */}
-      <SidebarContent>
-
-      </SidebarContent>
+      <SidebarContent></SidebarContent>
       {/* Footer */}
-      <SidebarFooter>
-        {/* <NavUser user={data.user} /> */}
-      </SidebarFooter>
+      <SidebarFooter>{/* <NavUser user={data.user} /> */}</SidebarFooter>
     </Sidebar>
-  )
+  );
 }
