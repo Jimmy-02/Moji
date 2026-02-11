44934b211b0cf0351857198799b13e3eddc9277e
Jimmy-02 <tainat1202@gmail.com>
2026-02-11 13:53:46 +0700

    feat: initialize chat window layout

--- frontend/src/components/chat/ChatWindowLayout.tsx
+++ frontend/src/components/chat/ChatWindowLayout.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useChatStore } from "@/stores/useChatStore";
+import ChatWelcomeScreen from "./ChatWelcomeScreen";
+import ChatWindowSkeleton from "./ChatWindowSkeleton";
 
 const ChatWindowLayout = () => {
+  const {
+    activeConversationId,
+    conversations,
+    messageLoading: loading,
+    markAsSeen,
+  } = useChatStore();
+
+  const selectedConvo = conversations.find((c) => c._id === activeConversationId) ?? null;
+
+  if (!selectedConvo) {
+    return <ChatWelcomeScreen />;
+  }
+
+  if (loading) {
+    return <ChatWindowSkeleton />;
+  }
+
   return (
     <div>ChatWindowLayout</div>
   )
