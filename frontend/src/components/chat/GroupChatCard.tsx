267a33aa1ce18d36b96023551c383e142a64b772
Jimmy-02 <tainat1202@gmail.com>
2026-02-11 12:47:26 +0700

    feat: complete leftsection and subtitle

--- frontend/src/components/chat/GroupChatCard.tsx
+++ frontend/src/components/chat/GroupChatCard.tsx
@@ -2,6 +2,8 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import { useChatStore } from "@/stores/useChatStore";
 import type { Conversation } from "@/types/chat";
 import ChatCard from "./ChatCard";
+import UnreadCountBadge from "./UnreadCountBadge";
+import GroupChatAvatar from "./GroupChatAvatar";
 
 const GroupChatCard = ({ convo }: { convo: Conversation }) => {
   const { user } = useAuthStore();
@@ -22,27 +24,30 @@ const GroupChatCard = ({ convo }: { convo: Conversation }) => {
     }
   };
 return (
-    <ChatCard
-      convoId={convo._id}
-      name={name}
-      timestamp={
-        convo.lastMessage?.createdAt
-          ? new Date(convo.lastMessage.createdAt)
-          : undefined
-      }
-      isActive={activeConversationId === convo._id}
-      onSelect={handleSelectConversation}
-      unreadCount={unreadCount}
-      leftSection={
-        <></>
-      }
-      subtitle={
-        <p className="text-sm truncate text-muted-foreground">
-          {convo.participants.length} thành viên
-        </p>
-      }
-    />
-  );
+  <ChatCard
+    convoId={convo._id}
+    name={name}
+    timestamp={
+      convo.lastMessage?.createdAt
+        ? new Date(convo.lastMessage.createdAt)
+        : undefined
+    }
+    isActive={activeConversationId === convo._id}
+    onSelect={handleSelectConversation}
+    unreadCount={unreadCount}
+    leftSection={
+      <>
+        {unreadCount > 0 && <UnreadCountBadge unreadCount={unreadCount} />}
+        <GroupChatAvatar participants={convo.participants} type="chat" />
+      </>
+    }
+    subtitle={
+      <p className="text-sm truncate text-muted-foreground">
+        {convo.participants.length} thành viên
+      </p>
+    }
+  />
+);
 };
 
 export default GroupChatCard
