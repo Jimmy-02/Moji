fea85a20acdf09547cc173a9206351d242f482be
Jimmy-02 <tainat1202@gmail.com>
2026-02-13 17:07:02 +0700

    feat: add custom scrollbar styling to sidebar

--- frontend/src/components/sidebar/app-sidebar.tsx
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -56,7 +56,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarMenu>
       </SidebarHeader>
       {/* Content */}
-      <SidebarContent>
+      <SidebarContent className="beautiful-scrollbar">
         {/* New chat */}
         <SidebarGroup>
           <CreateNewChat />
