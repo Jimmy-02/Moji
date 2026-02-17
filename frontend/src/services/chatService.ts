36d67946fb7968c68634a17d05b05ce29b8fa88f
Jimmy-02 <tainat1202@gmail.com>
2026-02-17 23:47:47 +0700

    feat: implement send group mesage api

--- frontend/src/services/chatService.ts
+++ frontend/src/services/chatService.ts
@@ -10,18 +10,22 @@ const pageLimit = 50;
 
 export const chatService = {
   async fetchConversations(): Promise<ConversationResponse> {
-        const res = await api.get("/conversations");
-        return res.data;
-    },
+    const res = await api.get("/conversations");
+    return res.data;
+  },
 
   async fetchMessages(id: string, cursor?: string): Promise<FetchMessageProps> {
-        const res = await api.get(
-          `/conversations/${id}/messages?limit=${pageLimit}&cursor=${cursor}`,
-        );
-        return { messages: res.data.messages, cursor: res.data.nextCursor };
-
-    },
-  async sendDirectMessage(recipientId: string, content: string = "", imgUrl?: string, conversationId?: string) {
+    const res = await api.get(
+      `/conversations/${id}/messages?limit=${pageLimit}&cursor=${cursor}`,
+    );
+    return { messages: res.data.messages, cursor: res.data.nextCursor };
+  },
+  async sendDirectMessage(
+    recipientId: string,
+    content: string = "",
+    imgUrl?: string,
+    conversationId?: string,
+  ) {
     const res = await api.post("/messages/direct", {
       recipientId,
       content,
@@ -30,5 +34,17 @@ export const chatService = {
     });
 
     return res.data.message;
-  }
+  },
+  async sendGroupMessage(
+    conversationId: string,
+    content: string = "",
+    imgUrl?: string,
+  ) {
+    const res = await api.post("/messages/group", {
+      conversationId,
+      content,
+      imgUrl,
+    });
+    return res.data.message;
+  },
 };
