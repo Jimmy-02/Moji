0ad53e2c369809a0cb2ce95a6694e4696cc4ed07
Jimmy-02 <tainat1202@gmail.com>
2026-02-17 23:28:59 +0700

    feat: implement emoji picker

--- frontend/src/components/chat/EmojiPicker.tsx
+++ frontend/src/components/chat/EmojiPicker.tsx
@@ -1,8 +1,30 @@
+import { useThemeStore } from "@/stores/useThemeStore";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Smile } from "lucide-react";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
 
-const EmojiPicker = () => {
+interface EmojiPicker{
+    onChange: (value: string) => void;
+}
+const EmojiPicker = ({onChange}: EmojiPicker) => {
+    const { isDark } = useThemeStore();
+
   return (
-    <div>EmojiPicker</div>
-  )
+    <Popover>
+      <PopoverTrigger className="cursor-pointer">
+        <Smile className="size-4" />
+      </PopoverTrigger>
+      <PopoverContent>
+        <Picker
+          theme={isDark ? "dark" : "light"}
+          data={data}
+          onEmojiSelect={(emoji: any) => onChange(emoji.native)}
+          emojiSize={24}
+        />
+      </PopoverContent>
+    </Popover>
+  );
 }
 
 export default EmojiPicker
