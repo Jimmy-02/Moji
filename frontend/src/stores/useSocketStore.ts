bf85fe0be2dd88ddd4d116eee7f101bfeec1f524
Jimmy-02 <tainat1202@gmail.com>
2026-02-18 14:40:38 +0700

    feat: implement connectsocket

--- frontend/src/stores/useSocketStore.ts
+++ frontend/src/stores/useSocketStore.ts
@@ -6,5 +6,26 @@ import type { SocketState } from "@/types/store";
 const baseURL = import.meta.env.VITE_SOCKET_URL;
 
 export const useSocketStore = create<SocketState>((set, get) => ({
-    
+    socket: null,
+
+    connectSocket: () =>{
+        const accessToken = useAuthStore.getState().accessToken;
+        const existingSocket = get().socket
+
+        if (existingSocket) return;
+
+        const socket: Socket = io(baseURL, {
+            auth: {token: accessToken},
+            transports: ["websocket"]
+        });
+
+        set({socket});
+
+        socket.on("connect", () =>{
+            console.log("Socket connected!")
+        });
+    },
+    disconnectSocket: () => {
+        
+    },
 }))
