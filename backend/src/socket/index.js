b967ec3f225a2dfb80c8e8313e22b0e920c5858e
Jimmy-02 <tainat1202@gmail.com>
2026-02-20 23:24:42 +0700

    feat: make room for each conversations

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -2,6 +2,7 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 import { socketAuthMiddleware } from "../middlewares/socketMiddleware.js";
+import { getUserConversationsForSocketIO } from "../controllers/conversationController.js";
 
 const app = express();
 
@@ -27,6 +28,10 @@ io.on("connection", async(socket) =>{
 
     io.emit("online-users", Array.from(onlineUsers.keys()));
 
+    const conversationIds = await getUserConversationsForSocketIO(user._id);
+    conversationIds.forEach((id)=>{
+        socket.join(id);
+    })
     socket.on("disconnect", () =>{
         onlineUsers.delete(user._id);
         io.emit("online-users", Array.from(onlineUsers.keys()));
