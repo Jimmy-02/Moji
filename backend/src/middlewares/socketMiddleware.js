aa5faaeaa2a7206913af97aa442de963d3bf320b
Jimmy-02 <tainat1202@gmail.com>
2026-02-20 22:20:50 +0700

    feat: initialize socket middleware

---
+++ backend/src/middlewares/socketMiddleware.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const socketAuthMiddleware = async (socket, next) => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
