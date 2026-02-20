960112611dfc191fc3cfe56333c728f6d70c4305
Jimmy-02 <tainat1202@gmail.com>
2026-02-20 22:32:21 +0700

    feat: implement socket middleware

--- backend/src/middlewares/socketMiddleware.js
+++ backend/src/middlewares/socketMiddleware.js
@@ -3,8 +3,27 @@ import User from "../models/User.js";
 
 export const socketAuthMiddleware = async (socket, next) => {
     try {
+        const token = socket.handshake.auth?.token;
 
+        if (!token) {
+            return next(new Error("Unauthorized - Token is not exist"));
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        if (!decoded) {
+            return next(new Error("Unauthorized - Token is not invalid or outdated"));
+        }
+
+        const user = await User.findById(decoded.userId).select("-hashedPassword",);
+        if (!user) {
+          return next(new Error("User not exist"));
+        }
+
+        socket.user = user;
+
+        next();
     } catch (error) {
-        
+        console.error("Error when verify JWT in socketMiddleware", error);
+        next(new Error("Unauthorized"));
     }
 }
