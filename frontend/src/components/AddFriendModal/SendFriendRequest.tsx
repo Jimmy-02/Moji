4b736e14f9bd07723b905f46654184a912be5057
Jimmy-02 <tainat1202@gmail.com>
2026-02-26 19:32:09 +0700

    feat: add props and form types to SendFriendRequest

--- frontend/src/components/AddFriendModal/SendFriendRequest.tsx
+++ frontend/src/components/AddFriendModal/SendFriendRequest.tsx
@@ -1,5 +1,14 @@
+import type { UseFormRegister } from "react-hook-form";
+import type { IFormValues } from "../chat/AddFriendModal";
 
 
+interface SendRequestProps {
+  register: UseFormRegister<IFormValues>;
+  loading: boolean;
+  searchedUsername: string;
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+  onBack: () => void;
+}
 
 const SendFriendRequest = () => {
   return (
