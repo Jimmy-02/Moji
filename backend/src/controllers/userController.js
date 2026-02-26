0b4a1c51353b93cbc4ee90590eeda00480e4065c
Jimmy-02 <tainat1202@gmail.com>
2026-02-26 18:20:12 +0700

    feat: implement search user by username api

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -8,3 +8,20 @@ export const authMe = async (req, res) => {
         return res.status(500).json({ message: "System error" });
     }
 }
+
+export const searchUserByUsername = async (req, res) => {
+    try {
+        const {username} = req.query;
+
+        if (!username || username.trim() === "") {
+            return res.status(400).json({ message: "Username is required" });
+        }
+
+        const user = await User.findOne({ username}).select("_id displayName username avatarUrl");
+        
+        return res.status(200).json({user});
+    } catch (error) {
+        console.log("Error searching user by username:", error);
+        return res.status(500).json({ message: "System error" });
+    }
+}
