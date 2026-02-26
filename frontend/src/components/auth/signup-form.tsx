430cb05cc24370fcc14c34db4441d8e0ee1453b7
Jimmy-02 <tainat1202@gmail.com>
2026-02-26 19:22:55 +0700

    style: apply custom css error message

--- frontend/src/components/auth/signup-form.tsx
+++ frontend/src/components/auth/signup-form.tsx
@@ -80,7 +80,7 @@ export function SignupForm({className,...props}: React.ComponentProps<"div">) {
                   <Label htmlFor="password" className="block text-sm">Mật khẩu</Label>
                   <Input type="password" id="password" {...register("password")}/>
                   {errors.password && (
-                    <p className="text-destructive text-sm"> {errors.password.message} </p>
+                    <p className="error-message"> {errors.password.message} </p>
                   )}
                 </div>
                 <Button type="submit" className="w-full cursor-pointer" disabled={isSubmitting}>Tạo tài khoản</Button>
