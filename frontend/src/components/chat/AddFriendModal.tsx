eb04eb0160323be1ae0b4b7f911ac4b0bf63cff5
Jimmy-02 <tainat1202@gmail.com>
2026-02-26 18:52:55 +0700

    feat: add AddFriendModal component with dialog UI

--- frontend/src/components/chat/AddFriendModal.tsx
+++ frontend/src/components/chat/AddFriendModal.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { UserPlus } from "lucide-react";
+
+export interface IFromValues{
+  username: string;
+  message: string;
+}
 
 const AddFriendModal = () => {
+  const [isFound, setIsFound] = useState<boolean | null>(null);
+
   return (
-    <div>AddFriendModal</div>
-  )
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="flex justify-center items-center size-5 rounded-full hover:bg-sidebar-accent cursor-pointer z-10">
+          <UserPlus className="size-4" />
+          <span className="sr-only">Kết bạn</span>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] border-none">
+        <DialogHeader>
+          <DialogTitle>Kết Bạn</DialogTitle>
+        </DialogHeader>
+        
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default AddFriendModal
