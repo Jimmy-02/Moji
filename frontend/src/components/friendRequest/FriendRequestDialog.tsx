f77eef54e7cc990ec242d7276cfab2e5b5a55b2e
Jimmy-02 <tainat1202@gmail.com>
2026-02-28 00:55:29 +0700

    feat: initialize friend request dialog

--- frontend/src/components/friendRequest/FriendRequestDialog.tsx
+++ frontend/src/components/friendRequest/FriendRequestDialog.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useFriendStore } from "@/stores/useFriendStore";
 
-const FriendRequestDialog = () => {
-  return (
-    <div>FriendRequestDialog</div>
-  )
+interface FriendRequestDialogProps {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export default FriendRequestDialog
+const FriendRequestDialog = ({ open, setOpen }: FriendRequestDialogProps) => {
+    const [tab, setTab] = useState("received");
+    const { getAllFriendRequests } = useFriendStore();
+
+    useEffect(() => {
+    const loadRequest = async () => {
+      try {
+        await getAllFriendRequests();
+      } catch (error) {
+        console.error("Error when loading friend request", error);
+      }
+    };
+
+    loadRequest();
+  }, []);
+
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-lg">
+          <DialogHeader>
+            <DialogTitle>Lời mời kết bạn</DialogTitle>
+          </DialogHeader>
+          <Tabs value={tab} onValueChange={setTab} className="w-full">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="received">Đã nhận</TabsTrigger>
+              <TabsTrigger value="sent">Đã gửi</TabsTrigger>
+            </TabsList>
+            <TabsContent value="received">Received</TabsContent>
+            <TabsContent value="sent">Sent</TabsContent>
+          </Tabs>
+        </DialogContent>
+      </Dialog>
+    );
+}
+
+export default FriendRequestDialog;
