749b69cafd9bcbc4b8dc55505c4bdac4766834d3
Jimmy-02 <tainat1202@gmail.com>
2026-02-28 16:52:18 +0700

    feat: initialize friend list modal

--- frontend/src/components/createNewChat/FriendListModal.tsx
+++ frontend/src/components/createNewChat/FriendListModal.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useFriendStore } from '@/stores/useFriendStore';
+import { DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
+import { MessageCircleMore } from 'lucide-react';
 
 const FriendListModal = () => {
+    const {friends} = useFriendStore();
   return (
-    <div>FriendListModal</div>
-  )
+    <DialogContent className="glass max-w-md">
+      <DialogHeader>
+        <DialogTitle className="flex items-center gap-2 text-xl capitalize">
+          <MessageCircleMore className="size-5" />
+          bắt đầu cuộc trò chuyện mới
+        </DialogTitle>
+      </DialogHeader>
+    </DialogContent>
+  );
 }
 
 export default FriendListModal
