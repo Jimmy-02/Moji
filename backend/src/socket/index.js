26028b6ec122bba46b4733aaf87e6375760bed3c
Jimmy-02 <tainat1202@gmail.com>
2026-03-04 04:52:48 +0700

    feat: implement join conversation

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -31,7 +31,13 @@ io.on("connection", async(socket) =>{
     const conversationIds = await getUserConversationsForSocketIO(user._id);
     conversationIds.forEach((id)=>{
         socket.join(id);
-    })
+    });
+
+    socket.on("join-conversation", (conversationId) =>{
+        socket.join(conversationId);
+        console.log(`User ${user.displayName} joined conversation ${conversationId}`);
+    });
+
     socket.on("disconnect", () =>{
         onlineUsers.delete(user._id);
         io.emit("online-users", Array.from(onlineUsers.keys()));
