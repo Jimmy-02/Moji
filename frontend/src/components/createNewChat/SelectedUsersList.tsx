9b8b77aa1f2be9c1388d89c818d57b2fea2f3982
Jimmy-02 <tainat1202@gmail.com>
2026-03-05 07:00:39 +0700

    feat: initial selected users list

--- frontend/src/components/createNewChat/SelectedUsersList.tsx
+++ frontend/src/components/createNewChat/SelectedUsersList.tsx
@@ -1,6 +1,15 @@
+import type { Friend } from '@/types/user';
 import React from 'react'
 
-const SelectedUsersList = () => {
+
+interface SelectedUsersListProps {
+  invitedUsers: Friend[];
+  onRemove: (user: Friend) => void;
+}
+const SelectedUsersList = ({ invitedUsers, onRemove }: SelectedUsersListProps) => {
+    if (invitedUsers.length === 0) {
+      return;
+    }
   return (
     <div>SelectedUsersList</div>
   )
