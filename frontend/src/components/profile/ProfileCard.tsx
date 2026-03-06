76ef1dd4018e83edbd3c677e4d0ff0c1249c5169
Jimmy-02 <tainat1202@gmail.com>
2026-03-06 23:32:38 +0700

    feat: implement user avatar

--- frontend/src/components/profile/ProfileCard.tsx
+++ frontend/src/components/profile/ProfileCard.tsx
@@ -1,10 +1,30 @@
 import type { User } from "@/types/user";
+import { Card, CardContent } from "../ui/card";
+import UserAvatar from "../chat/UserAvatar";
 
 interface ProfileCardProps {
   user: User | null;
 }
 const ProfileCard = ({ user }: ProfileCardProps) => {
-  return <div>ProfileCard</div>;
+  if (!user) return;
+
+  if (!user.bio) {
+    user.bio = "Will code for eggs";
+  }
+  return (
+    <Card className="overflow-hidden p-0 h-52 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <CardContent className="mt-20 pb-8 flex flex-col sm:flex-row items-center sm:items-end gap-6">
+        <div className="relative">
+          <UserAvatar
+            type="profile"
+            name={user.displayName}
+            avatarUrl={user.avatarUrl ?? undefined}
+            className="ring-4 ring-white shadow-lg"
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ProfileCard
