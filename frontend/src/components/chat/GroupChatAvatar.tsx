2e77f84b4bfeb4afcfccfda21275d6378732eb92
Jimmy-02 <tainat1202@gmail.com>
2026-03-06 23:04:20 +0700

    fix: remove unused semicolon

--- frontend/src/components/chat/GroupChatAvatar.tsx
+++ frontend/src/components/chat/GroupChatAvatar.tsx
@@ -32,7 +32,7 @@ const GroupChatAvatar = ({ participants, type }: GroupChatAvatarProps) => {
         <div className="flex items-center z-10 justify-center size-8 rounded-full bg-muted ring-2 ring-background text-muted-foreground">
           <Ellipsis className="size-4" />
         </div>
-      )};
+      )}
     </div>
   );
 };
