11d5e90c471fdae20b0b623bd62e9419f5d8ab85
Jimmy-02 <tainat1202@gmail.com>
2026-03-08 00:32:45 +0700

    feat: add avatar uploader

--- frontend/src/components/profile/ProfileCard.tsx
+++ frontend/src/components/profile/ProfileCard.tsx
@@ -4,6 +4,8 @@ import UserAvatar from "../chat/UserAvatar";
 import { Badge } from "../ui/badge";
 import { cn } from "@/lib/utils";
 import { useSocketStore } from "@/stores/useSocketStore";
+import { Avatar } from "radix-ui";
+import AvatarUploader from "./AvatarUploader";
 
 interface ProfileCardProps {
   user: User | null;
@@ -29,6 +31,7 @@ const ProfileCard = ({ user }: ProfileCardProps) => {
             avatarUrl={user.avatarUrl ?? undefined}
             className="ring-4 ring-white shadow-lg"
           />
+          <AvatarUploader/>
         </div>
 
         {/* user info */}
