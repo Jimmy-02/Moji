2f69a3e622c3edde18fda140c8480c085232ec9f
Jimmy-02 <tainat1202@gmail.com>
2026-03-07 22:50:24 +0700

    feat: implement upload image buffer

--- backend/src/middlewares/uploadMiddleware.js
+++ backend/src/middlewares/uploadMiddleware.js
@@ -6,3 +6,25 @@ export const upload = multer({
     fileSize: 1024 * 1024 * 1, // 1MB
   },
 });
+
+export const uploadImageFromBuffer = (buffer, options) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: "moji_chat/avatars",
+        resource_type: "image",
+        transformation: [{ width: 200, height: 200, crop: "fill" }],
+        ...options,
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      },
+    );
+
+    uploadStream.end(buffer);
+  });
+};
