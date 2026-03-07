ee258bddb6147d88a937d76d550417721f3cfdd8
Jimmy-02 <tainat1202@gmail.com>
2026-03-08 00:26:15 +0700

    feat: implement button as camera ui

--- frontend/src/components/profile/AvatarUploader.tsx
+++ frontend/src/components/profile/AvatarUploader.tsx
@@ -1,11 +1,24 @@
+import { useUserStore } from '@/stores/useUserStore';
+import { Camera } from 'lucide-react';
 import { useRef } from 'react'
+import { Button } from '../ui/button';
 
 const AvatarUploader = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const { updateAvatarUrl } = useUserStore();
 
   return (
-    <div>AvatarUploader</div>
-  )
+    <>
+      <Button
+        size="icon"
+        variant="secondary"
+        onClick={() => {}}
+        className="absolute -bottom-2 -right-2 size-9 rounded-full shadow-md hover:scale-115 transition duration-300 hover:bg-background"
+      >
+        <Camera className="size-4" />
+      </Button>
+    </>
+  );
 }
 
 export default AvatarUploader
