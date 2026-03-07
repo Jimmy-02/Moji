c100fa5dc65d4b94172c18b629a45bab43f52c24
Jimmy-02 <tainat1202@gmail.com>
2026-03-08 00:20:53 +0700

    feat: initial avatar uploader ui

---
+++ frontend/src/components/profile/AvatarUploader.tsx
@@ -0,0 +1,11 @@
+import { useRef } from 'react'
+
+const AvatarUploader = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div>AvatarUploader</div>
+  )
+}
+
+export default AvatarUploader
