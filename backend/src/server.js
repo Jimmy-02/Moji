0b0ce88fb170c75506718b6e7ecea4b2caa097a1
Jimmy-02 <tainat1202@gmail.com>
2026-03-07 22:39:35 +0700

    feat: initial cloudinary

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser'
 import { protectedRoute } from './middlewares/authMiddleware.js';
 import cors from "cors"
 import { app, server } from "./socket/index.js";
-
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
@@ -19,6 +19,12 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use('/api/auth', authRoute);
 
